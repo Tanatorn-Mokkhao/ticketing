41df07b90eff5c8fa19997eb45c3e35a419c06ef
Tanatorn Mokkhao <tanatorn.mokkhao@gmail.com>
2022-04-28 16:16:16 +0700

    upda front

--- client/component/header.js
+++ client/component/header.js
@@ -2,22 +2,23 @@ import Link from "next/link";
 
 export default ({ currentUser }) => {
   const links = [
-    !currentUser && { label: "Sign Up", href: "/auth/signup" },
-    !currentUser && { label: "Sign In", href: "/auth/signin" },
+    !currentUser && { label: 'Sign Up', href: '/auth/signup' },
+    !currentUser && { label: 'Sign In', href: '/auth/signin' },
     currentUser && { label: 'Sell Tickets', href: '/tickets/new' },
-    currentUser && {label: 'My Orders', href: 'orders'},
-    currentUser && { label: "Sign Out", href: "/auth/signout" },
+    currentUser && { label: 'My Orders', href: '/orders' },
+    currentUser && { label: 'Sign Out', href: '/auth/signout' },
   ]
     .filter((linkConfig) => linkConfig)
     .map(({ label, href }) => {
       return (
         <li key={href} className="nav-item">
           <Link href={href}>
-            <a className="navbar-link">{label}</a>
+            <a className="nav-link">{label}</a>
           </Link>
         </li>
       );
     });
+
   return (
     <nav className="navbar navbar-light bg-light">
       <Link href="/">
